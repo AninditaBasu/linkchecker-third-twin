c1b8678e7012ab45f630d0c8d9933ff844ef4499
Anindita Basu <anindita.basu@live.com>
2016-05-17 19:43:20 +0530

    licensing info

--- third_twin.py
+++ third_twin.py
@@ -1,5 +1,23 @@
 #
 #
+# third_twin.py
+# Copyright (C) 2015  Anindita Basu
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see http://www.gnu.org/licenses/gpl-3.0.txt.
+#
+#
+#
 # In a workspace, if a DITA file is linked to another file multiple times, this program will find those instances and report them.
 # This program will look at ditamap files for links through parent:child nesting, relationship tables, and family collections.
 # This program will also look at dita files for related links and inline links.
